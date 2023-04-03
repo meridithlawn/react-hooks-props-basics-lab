ed1e864578398f557aa1524638e76ba13ae6d820
Meridith Lawn <meridithlawn2@gmail.com>
2023-04-03 12:17:49 -0600

    commit tree 2f078dccd70b096ae2f9a48351e311c0ec6f79dc

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+
 function Home(props) {
+  console.log("props", props)
   return (
     <div id="home">
       <h1 style={{ color: props.color }}>
-        {props.name} is a Web Developer from {props.city}
+        {props.username} is a Web Developer from {props.city}
       </h1>
     </div>
   );
